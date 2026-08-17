a5cd315d7b8a94ac62d5bc07a909f870449c0e9a
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-17 13:54:26 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,11 +1,11 @@
 -- PROGRAM 3: ALTER STUDENT TABLE
--- Student Name:
--- Register Number:
 
 USE CollegeDB;
 
 -- Add Email VARCHAR(30)
 
--- Add PhoneNumber NUMBER(10)
 
--- Display the modified table structure
+-- Add PhoneNumber
+
+
+-- Display modified table structure
