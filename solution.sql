42f898f70706253401376fc0d7f7a43b4b57e93e
RUBINA PARVEEN S <rubinaperveen2022@gmail.com>
2026-08-24 23:18:05 +0530

    program 3 rdbms 1U25IT080

--- solution.sql
+++ solution.sql
@@ -1,1 +1,14 @@
+DROP DATABASE IF EXISTS CollegeDB;
 
+CREATE DATABASE CollegeDB;
+
+USE CollegeDB;
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20),
+    DOB DATE,
+    Gender VARCHAR(10),
+    DepartmentID INT
+);
+DESC Student;
